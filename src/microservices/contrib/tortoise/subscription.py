a00974703403642bcf95700dd4ab30c9faf5b6a6
bgswe <bg.soft.eng@gmail.com>
2022-07-22 13:26:11 -0700

    Move sub repo to microservices

--- src/microservices/contrib/tortoise/subscription.py
+++ src/microservices/contrib/tortoise/subscription.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from tortoise import fields
 
 from microservices.contrib.tortoise import AbstractBaseModel
-from microservices.domain import EventStreams
+from microservices.domain import EventStreams, Subscription
+from microservices.repository import AsyncRepository
 
 
 class SubscriptionORM(AbstractBaseModel):
@@ -11,3 +14,34 @@ class SubscriptionORM(AbstractBaseModel):
 
     class Meta:
         table = "subscription"
+
+
+class TortoiseSubscriptionRepository(AsyncRepository):
+    async def _get_list(self) -> List[Subscription]:
+        sub_orms = await SubscriptionORM.all()
+
+        sub_list = [
+            Subscription(
+                id=s.id,
+                stream=s.stream,
+                webhook_url=s.webhook_url,
+                health_check_url=s.health_check_url,
+            )
+            for s in sub_orms
+        ]
+
+        return sub_list
+
+    async def _get():
+        pass
+
+    async def _add(self, sub: Subscription):
+        await SubscriptionORM.create(
+            id=sub.id,
+            stream=sub.stream,
+            webhook_url=sub.webhook_url,
+            health_check_url=sub.health_check_url,
+        )
+
+    async def _update():
+        pass
