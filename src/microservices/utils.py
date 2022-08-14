f9b212e98bbb8ba9bfb8d4e4fd64e5be0163a63f
bgswe <bg.soft.eng@gmail.com>
2022-08-13 23:09:05 -0700

    Add CI workflow basics

--- src/microservices/utils.py
+++ src/microservices/utils.py
@@ -7,10 +7,18 @@ from structlog.processors import JSONRenderer
 
 def uuid4() -> str:
     """Reduces uuid lib uuid4 generation to str type."""
+
     return str(uuid.uuid4())
 
 
 def get_logger():
+    """Provides a structlog logger for use anywhere.
+
+    EVAL: It's undecided whether any logger abstraction is necessary
+    here. We could define a logger interface and then provide wrapper
+    a wrapper class for the current implementation.
+    """
+
     configure_once(processors=[JSONRenderer(indent=2, sort_keys=True)])
 
     return get_structlog_logger()
