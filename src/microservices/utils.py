f676610540de8112b768b361cfc72e45429b2831
bgswe <bg.soft.eng@gmail.com>
2022-10-01 11:51:29 -0700

    Add structlog ability to filter log levels

--- src/microservices/utils.py
+++ src/microservices/utils.py
@@ -1,27 +1,33 @@
+import logging
+import warnings
 from uuid import UUID, uuid4
 
-from structlog import configure
-from structlog import get_logger as get_structlog_logger
+from structlog import configure_once, get_logger, make_filtering_bound_logger  # noqa
 from structlog.processors import JSONRenderer
+
+log_level_map = {
+    "DEBUG": 10,
+    "INFO": 20,
+    "WARN": 30,
+    "ERROR": 40,
+    "CRITICAL": 50,
+}
+
+
+def configure_logs(log_level: int = logging.ERROR):
+    """..."""
+
+    # Raises runing warning one addition config attempts, ignore it
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+
+        configure_once(
+            wrapper_class=make_filtering_bound_logger(log_level),
+            processors=[JSONRenderer(indent=2, sort_keys=True)],
+        )
 
 
 def get_uuid() -> UUID:
     """Reduces uuid lib uuid4 generation to str type."""
 
     return uuid4()
-
-
-configured = False
-
-
-def get_logger():
-    """Provides a structlog logger for use anywhere.
-
-    EVAL: It's undecided whether any logger abstraction is necessary
-    here. We could define a logger interface and then provide wrapper
-    a wrapper class for the current implementation.
-    """
-    if not configured:
-        configure(processors=[JSONRenderer(indent=2, sort_keys=True)])
-
-    return get_structlog_logger()
