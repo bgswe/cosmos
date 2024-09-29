1d364322296ce138d8cc33c78b1cd90a672ecb59
bgswe <bg.soft.eng@gmail.com>
2024-09-28 20:39:12 -0700

    ADD SOME LOGS

--- src/cosmos/contrib/pg/transactional_outbox.py
+++ src/cosmos/contrib/pg/transactional_outbox.py
@@ -3,15 +3,26 @@ import pickle
 import asyncpg
 from cosmos.domain import Message
 
+import structlog
+
+logger = structlog.get_logger()
+
 
 class PostgresOutbox:
     def __init__(self):
         self.connection: asyncpg.Connection | None = None
 
     async def send(self, messages: list[Message]):
+        logger.info("PostgresOutbox SEND")
+
         if self.connection is None:
             # TODO: Raise custom exception
             raise Exception("This outbox requires a connection object before delivery")
+
+        logger.info("DESCRIBING MESSAGES")
+        for message in messages:
+            log = logger.bind(message_id=message, message_name=message.type_name)
+            log.info("message")
 
         # TODO: batch insert the list of messages
         for message in messages:
